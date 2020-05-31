5ba2a0efa0da7a2478bc8451f28b19fd532e2929
James Harmon <james.harmon@footlocker.com>
2020-05-31 16:44:01 -0500

    replace plugin with table

--- desktop/plugins/fl_glide/src/index.tsx
+++ desktop/plugins/fl_glide/src/index.tsx
@@ -1,60 +1,140 @@
+import {
+  Text,
+  Panel,
+  ManagedDataInspector,
+  FlipperPlugin,
+  DetailSidebar,
+  FlexRow,
+  FlexColumn,
+  styled,
+  colors,
+} from 'flipper';
 import React from 'react';
-import { FlipperPlugin, FlexColumn, KeyboardActions } from 'flipper';
 
-type State = {};
+type Id = number;
 
-type Data = {};
-
-type PersistedState = {
-  data: Array<Data>;
+type Row = {
+  id: Id;
+  title: string;
+  url: string;
 };
 
-export default class extends FlipperPlugin<State, any, PersistedState> {
+function renderSidebar(row: Row) {
+  return (
+    <Panel floating={false} heading={'Extras'}>
+      <ManagedDataInspector data={row} expandRoot={true} />
+    </Panel>
+  );
+}
 
-  static title = 'FL Glide Plugin'
-  static id = 'fl-glide'
-  //static icon = 'app'
+type State = {
+  selectedID: string | null;
+};
 
-  componentDidMount() {
-    this.client.subscribe('message', message => {
-      this.setState({ message })
-      console.log(this.state.message)
-    }
-    )
-  }
+type PersistedState = {
+  [key: string]: Row;
+};
 
+export default class GlideImagePlugin extends FlipperPlugin<
+  State,
+  any,
+  PersistedState
+  > {
+  static defaultPersistedState = {};
 
-  static keyboardActions: KeyboardActions = ['clear'];
-
-  static defaultPersistedState: PersistedState = {
-    data: [],
-  };
-
-  static persistedStateReducer = (
+  static persistedStateReducer<PersistedState>(
     persistedState: PersistedState,
     method: string,
-    data: Data,
-  ): PersistedState => {
-    return {
-      ...persistedState,
-      data: persistedState.data.concat([data]),
-    };
-  };
-
-  state = {};
-
-  onKeyboardAction = (action: string) => {
-    if (action === 'clear') {
-      this.props.setPersistedState({ data: [] });
+    payload: Row,
+  ) {
+    console.log('processing new image call')
+    if (method === 'newImage') {
+      return Object.assign({}, persistedState, {
+        [payload.id]: payload,
+      });
     }
+    return persistedState;
+  }
+
+  static Container = styled(FlexRow)({
+    backgroundColor: colors.macOSTitleBarBackgroundBlur,
+    flexWrap: 'wrap',
+    alignItems: 'flex-start',
+    alignContent: 'flex-start',
+    flexGrow: 1,
+    overflow: 'scroll',
+  });
+
+  state = {
+    selectedID: null as string | null,
   };
 
   render() {
+    const { selectedID } = this.state;
+    const { persistedState } = this.props;
+
     return (
-      <FlexColumn>
-        <div>Hello World</div>
-      </FlexColumn>
+      <GlideImagePlugin.Container>
+        {Object.entries(persistedState).map(([id, row]) => (
+          <Card
+            {...row}
+            onSelect={() => this.setState({ selectedID: id })}
+            selected={id === selectedID}
+            key={id}
+          />
+        ))}
+        <DetailSidebar>
+          {selectedID && renderSidebar(persistedState[selectedID])}
+        </DetailSidebar>
+      </GlideImagePlugin.Container>
     );
   }
+}
 
+class Card extends React.Component<
+  {
+    onSelect: () => void;
+    selected: boolean;
+  } & Row
+  > {
+  static Container = styled(FlexColumn)<{ selected?: boolean }>((props) => ({
+    margin: 10,
+    borderRadius: 5,
+    border: '2px solid black',
+    backgroundColor: colors.white,
+    borderColor: props.selected
+      ? colors.macOSTitleBarIconSelected
+      : colors.white,
+    padding: 0,
+    width: 150,
+    overflow: 'hidden',
+    boxShadow: '1px 1px 4px rgba(0,0,0,0.1)',
+    cursor: 'pointer',
+  }));
+
+  static Image = styled.div({
+    backgroundSize: 'cover',
+    width: '100%',
+    paddingTop: '100%',
+  });
+
+  static Title = styled(Text)({
+    fontSize: 14,
+    fontWeight: 'bold',
+    padding: '10px 5px',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    whiteSpace: 'nowrap',
+  });
+
+  render() {
+    return (
+      <Card.Container
+        onClick={this.props.onSelect}
+        selected={this.props.selected}>
+        <Card.Image style={{ backgroundImage: `url(${this.props.url || ''})` }} />
+        <Card.Title>{this.props.title}</Card.Title>
+      </Card.Container>
+    );
+  }
 }

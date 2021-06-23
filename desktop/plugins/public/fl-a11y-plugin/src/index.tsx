96533973da030a21544cc08a6b5debebc56f33d7
bizzguy <jamesharmon@gmail.com>
2021-06-23 00:34:42 -0500

    working Row/Col

--- desktop/plugins/public/fl-a11y-plugin/src/index.tsx
+++ desktop/plugins/public/fl-a11y-plugin/src/index.tsx
@@ -10,6 +10,12 @@ import {
   usePlugin,
 } from 'flipper-plugin';
 
+import {
+  Button,
+  Col, 
+  Row
+} from 'antd'
+
 type DataRow = {
   id: number;
   time: string;
@@ -20,6 +26,9 @@ type DataRow = {
 type Events = {
   newRow: DataRow;
 };
+
+const style = { background: '#ffffff', padding: '6px 0' };
+const textStyle = { background: '#ffffff', padding: '6px 0' };
 
 function createRow(event: DataRow) {
   return {
@@ -37,6 +46,14 @@ export function plugin(client: PluginClient<Events, {}>) {
   client.onMessage('newRow', (payload) => {
     rows.append(createRow(payload));
   });
+
+  const talkbackOn = () => {
+    //new TalkbackOnCommand(adbBridge).execute();
+  }
+
+  const talkbackOff = () => {
+    //new TalkbackOffCommand(adbBridge).execute();
+  }
 
   const columns: DataTableColumn<DataRow>[] = [
     {
@@ -56,6 +73,8 @@ export function plugin(client: PluginClient<Events, {}>) {
   return {
     rows,
     columns,
+    talkbackOn,
+    talkbackOff,
   };
 }
 
@@ -63,8 +82,33 @@ export function Component() {
   const instance = usePlugin(plugin);
   return (
     <Layout.Container grow>
-      <Layout.Container>
-
+      <Layout.Container gap>
+        <Row gutter={8}>
+          <Col className="gutter-row" span={4}>
+            <div style={textStyle}>
+            Talk Back
+            </div>
+          </Col>
+          <Col className="gutter-row" span={16}>
+            <div style={style}>
+              <Button type="primary" onClick={instance.talkbackOn}>Talkback On</Button>
+              <Button type="primary" onClick={instance.talkbackOff}>Talkback Off</Button>
+            </div>
+          </Col>
+        </Row>
+        <Row gutter={8}>
+          <Col className="gutter-row" span={4}>
+            <div style={textStyle}>
+            Talk Back
+            </div>
+          </Col>
+          <Col className="gutter-row" span={16}>
+            <div style={style}>
+              <Button type="primary" onClick={instance.talkbackOn}>Talkback On</Button>
+              <Button type="primary" onClick={instance.talkbackOff}>Talkback Off</Button>
+            </div>
+          </Col>
+        </Row>
       </Layout.Container>
       <Layout.Container grow>
           <DataTable<DataRow>
